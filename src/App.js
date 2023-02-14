796220ff732f38837eff046a806f6c660050d416
Patrick Osten <posten.coding@gmail.com>
2023-02-13 16:06:19 -0800

    remove comments

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import TheBtn from './components/thebtn';
-
-import {useEffect, useState, useMemo} from 'react'; 
+import {useEffect, useState} from 'react'; 
 import './App.css';
 
 const center = computeCenter([window.innerHeight, window.innerWidth])
 function computeCenter(windowYX){
   let r = 0;
   windowYX[1]>windowYX[0]?
-  r = windowYX[0]
-  :
-  r = windowYX[1];
+  r = windowYX[0] : r = windowYX[1];
   const center = (r/2)
-  //const center = 3.14 * (center**2)
   return center;
 }
-
 function App() {
-  // let windowSize = useWindowSize();
   const [mousePos, setMousePos] = useState({});
   useEffect(() => {
     const handleMouseMove = (event) => {
--- src/components/thebtn.js
+++ src/components/thebtn.js
@@ -1,14 +1,11 @@
 import Button from 'react-bootstrap/Button';
 import Popover from 'react-bootstrap/Popover';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import {useSpring, animated} from '@react-spring/web';
 
 function computePos(pos, center){
-  if(pos > center){
-    //const r = Math.floor(Math.random() * 4)
-    return (center%4)/2
-  }
+  if(pos > center) return (center%4)/2
   if(pos < 5) return (center%4)/2
   if(pos < center && pos > 5) return pos+(center/2)
 }
@@ -21,14 +18,8 @@ const popover = (
   </Popover>
 );
 const TheBtn = (props)=>{
-  //console.log(props)
   const {mouseX, mouseY, center} = props;
   const [current, setCurrent] = useState({x:mouseX, y:mouseY})
-  // useEffect(()=> {
-  //   setTimeout(()=>{
-  //     console.log("time!")
-  //   }, 3000)
-  // })
   const [position, api] = useSpring(() => ({
     from: {
       top: current.y,
@@ -38,8 +29,6 @@ const TheBtn = (props)=>{
       top: computePos(mouseX, center),
       left: computePos(mouseY, center)}
   }), [mouseX, mouseY])
-  
-  //console.log(api)
   return (
     <>
     <animated.div id="animated-btn" style={position}>
@@ -47,7 +36,7 @@ const TheBtn = (props)=>{
       <Button
         variant="primary"
         size="lg"
-      >X:{mouseX}, Y:{mouseY}</Button>
+      >CLICK HERE</Button>
       </OverlayTrigger>
     </animated.div>
     </>
